1be0d9632a4e9ceb9b50960d268788876accdd76
nikitaarora18 <nikitaaror6918@gmail.com>
2025-09-15 18:38:01 +0530

    Add files via upload

---
+++ datatypes1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+ printf("Size of char: %d byte(s)\n", sizeof(char));
+    printf("Size of int: %d byte(s)\n", sizeof(int));
+    printf("Size of long: %d byte(s)\n", sizeof(long));
+    printf("Size of float: %d byte(s)\n", sizeof(float));
+    printf("Size of double: %d byte(s)\n", sizeof(double));
+    printf("Size of long double: %d byte(s)\n", sizeof(long double));
+    return 0;
+}
